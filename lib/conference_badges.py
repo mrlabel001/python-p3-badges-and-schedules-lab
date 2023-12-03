014cc2a323edec6a762b79d02d361448cc690631
mrlabel001 <farbean.makini@student.gmail.com>
2023-12-03 04:37:40 +0300

    completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    names = ['Guido', 'Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus','Matz']
+    rooms = range(1,9)
+    room_assignment = []
+    for i, name in enumerate (names):
+        rooms_message = f"Hello, {name}! You'll be assigned to room {rooms[i]}!"
+        room_assignment.append(rooms_message)
+    return room_assignment
 
 def printer(names):
-    return None
+    badge = batch_badge_creator(names)
+    room_assign = assign_rooms(names)
+    for badges in badge:
+        print(badges)
+    for rooms_assigned in room_assign:
+        print(rooms_assigned)
+    
